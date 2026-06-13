d30536955e286be0817224818817be93ac7c85c9
Edward Sia <edwardskt.general@gmail.com>
2026-06-14 00:19:39 +1000

    Add: roman-to-integer - submission-0

---
+++ Data Structures & Algorithms/roman-to-integer/submission-0.ts
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    romanToInt(s: string): number {
+
+        const roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        let num = 0
+
+        for (let i = 0 ; i < s.length ; i++) {
+            const curr = roman[s[i]]
+            const next = roman[s[i+1]]
+
+            if (next > curr) {
+                num -= curr
+            } else {
+                num += curr
+            }
+        }
+
+        return num
+    }
+}
