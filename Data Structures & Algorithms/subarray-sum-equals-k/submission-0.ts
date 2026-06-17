a68c9ce4d315ef7a8826461cef4b9ade25459d3b
Edward Sia <edwardskt.general@gmail.com>
2026-06-17 12:36:06 +1000

    Add: subarray-sum-equals-k - submission-0

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    subarraySum(nums: number[], k: number): number {
+        let res = 0
+        for (let i = 0 ; i < nums.length ; i++) {
+            let sum = 0
+            for (let j = i ; j < nums.length ; j++) {
+                sum += nums[j]
+                if (sum == k) res++
+            }
+        }
+
+        return res
+    }
+}
