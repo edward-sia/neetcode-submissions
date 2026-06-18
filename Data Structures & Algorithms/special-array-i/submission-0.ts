a4e1341ed933ff11316794b3f2a4c91881082996
Edward Sia <edwardskt.general@gmail.com>
2026-06-18 23:35:39 +1000

    Add: special-array-i - submission-0

---
+++ Data Structures & Algorithms/special-array-i/submission-0.ts
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    isArraySpecial(nums: number[]): boolean {
+        for (let i = 1 ; i < nums.length ; i++) {
+            if (nums[i-1] % 2 == nums[i] % 2) {
+                return false
+            }
+        }
+        return true
+    }
+}
