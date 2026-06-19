3b914319eca9a46718dffa5993d3efc9512f7672
Edward Sia <edwardskt.general@gmail.com>
2026-06-19 11:35:52 +1000

    Add: find-the-index-of-the-first-occurrence-in-a-string - submission-0

---
+++ Data Structures & Algorithms/find-the-index-of-the-first-occurrence-in-a-string/submission-0.ts
@@ -0,0 +1,10 @@
+class Solution {
+    /**
+     * @param {string} haystack
+     * @param {string} needle
+     * @return {number}
+     */
+    strStr(haystack: string, needle: string): number {
+        return haystack.indexOf(needle)
+    }
+}
