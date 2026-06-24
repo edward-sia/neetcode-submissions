9d857c83db523125ee7d8c76cad4df941a4b5a22
Edward Sia <edwardskt.general@gmail.com>
2026-06-24 16:50:29 +1000

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.ts
@@ -0,0 +1,37 @@
+/**
+ * // Definition for a Node.
+ * class Node {
+ *     constructor(val = 0, neighbors = []) {
+ *       this.val = val;
+ *       this.neighbors = neighbors;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Node} node
+     * @return {Node}
+     */
+    cloneGraph(node: Node | null): Node {
+        if (node === null) return null
+
+        let oldToNew = new Map<Node, Node>()
+        
+
+        return this.dfs(node, oldToNew)
+    }
+
+    dfs(oldN: Node | null, oldToNew: Map<Node, Node>): Node | null {
+        if (oldN === null) return null
+        if (oldToNew.has(oldN)) return oldToNew.get(oldN)
+
+        let newN = new Node(oldN.val, [])
+        oldToNew.set(oldN, newN)
+        
+        for (let nei of oldN.neighbors) {
+            newN.neighbors.push(this.dfs(nei, oldToNew))
+        }
+        return newN
+    }
+}
