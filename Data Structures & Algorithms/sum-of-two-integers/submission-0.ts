d9533fa098cfc6bb450197e70247d9e69ff655d2
Edward Sia <edwardskt.general@gmail.com>
2026-06-30 00:32:07 +1000

    Add: sum-of-two-integers - submission-0

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-0.ts
@@ -0,0 +1,10 @@
+class Solution {
+    /**
+     * @param {number} a
+     * @param {number} b
+     * @return {number}
+     */
+    getSum(a: number, b: number): number {
+        return a + b
+    }
+}
